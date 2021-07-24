7082bd1e4925b36a4d14782c3dfc6b183bbe8296
Aaron Li-Mai <aaronmai@hotmail.com>
2021-07-23 20:50:02 -0600

    added jackpot slash command

---
+++ dist/slashCommands/gambling/jackpot.js
@@ -0,0 +1,49 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("../../utils/utils");
+const discord_js_1 = require("discord.js");
+exports.default = {
+    name: "jackpot",
+    description: "Check the current jackpot amount.",
+    testOnly: true,
+    cooldown: 15,
+    clientPerms: ["SEND_MESSAGES", "EMBED_LINKS"],
+    execute({ client, interaction }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const guildInfo = yield utils_1.getGuildInfo(client, interaction.guild.id);
+            const { gamblingChannel, jackpotAmount } = guildInfo.gambling;
+            if (gamblingChannel) {
+                if (interaction.channel.id !== gamblingChannel) {
+                    return yield interaction.reply({
+                        content: `You can only check the jackpot in <#${gamblingChannel}>!`,
+                        ephemeral: true
+                    });
+                }
+            }
+            else {
+                return yield interaction.reply({
+                    content: "A gambling channel needs to be set first in order for this command to be used.",
+                    ephemeral: true
+                });
+            }
+            utils_1.setCooldown(client, this, interaction);
+            const msgEmbed = new discord_js_1.MessageEmbed()
+                .setColor("#85bb65")
+                .setTitle("Current Jackpot Amount")
+                .setThumbnail("https://i.imgur.com/VwbWTOn.png")
+                .setDescription(`The current jackpot amount is \`${utils_1.formatNumber(jackpotAmount)}\`.`)
+                .setFooter(`Requested by ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
+                .setTimestamp();
+            return yield interaction.reply({ embeds: [msgEmbed] });
+        });
+    }
+};
---
+++ src/slashCommands/gambling/jackpot.ts
@@ -0,0 +1,42 @@
+import { SlashCommand } from "../../interfaces/SlashCommand";
+import { getGuildInfo, formatNumber, setCooldown } from "../../utils/utils";
+import { MessageEmbed } from "discord.js";
+
+export default {
+    name: "jackpot",
+    description: "Check the current jackpot amount.",
+    testOnly: true,
+    cooldown: 30,
+    clientPerms: ["SEND_MESSAGES", "EMBED_LINKS"],
+    async execute({ client, interaction }) {
+        const guildInfo = await getGuildInfo(client, interaction.guild!.id);
+        const { gamblingChannel, jackpotAmount } = guildInfo.gambling;
+        if (gamblingChannel) {
+            if (interaction.channel!.id !== gamblingChannel) {
+                return await interaction.reply({
+                    content: `You can only check the jackpot in <#${gamblingChannel}>!`,
+                    ephemeral: true
+                });
+            }
+        } else {
+            return await interaction.reply({
+                content:
+                    "A gambling channel needs to be set first in order for this command to be used.",
+                ephemeral: true
+            });
+        }
+
+        setCooldown(client, this, interaction);
+        const msgEmbed = new MessageEmbed()
+            .setColor("#85bb65")
+            .setTitle("Current Jackpot Amount")
+            .setThumbnail("https://i.imgur.com/VwbWTOn.png")
+            .setDescription(`The current jackpot amount is \`${formatNumber(jackpotAmount!)}\`.`)
+            .setFooter(
+                `Requested by ${interaction.user.tag}`,
+                interaction.user.displayAvatarURL({ dynamic: true })
+            )
+            .setTimestamp();
+        return await interaction.reply({ embeds: [msgEmbed] });
+    }
+} as SlashCommand;
